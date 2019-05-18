e7d92e6df33dc2f509a4fb1db6e230e27efa3877
Adam Draper <adamwdraper@gmail.com>
2019-05-18 00:26:19 -0700

    Revert "Revert "first commit""
    This reverts commit 0889f99a248704e8f7de9860f57027a53f7f2a6e.

---
+++ src/components/Stats.vue
@@ -0,0 +1,25 @@
+<template>
+  <Stats>
+    <stat>
+      <name>Record</name>
+      <value>{{ stats.wins }} - {{ stats.losses }}</value>
+    </stat>
+  </Stats>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+
+  export default {
+    computed: {
+      ...mapGetters([
+        'stats'
+      ])
+    }
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Stats/>
+  </div>
+</template>
+
+<script>
+  // @ is an alias to /src
+  import Stats from '@/components/Stats.vue'
+
+  export default {
+    components: {
+      Stats
+    }
+  }
+</script>
